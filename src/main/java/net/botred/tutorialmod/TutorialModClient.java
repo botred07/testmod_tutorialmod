da08a3c25db0d6fd97db90db35b3250253a56ca1
Abraham <79951343+botred@users.noreply.github.com>
2025-11-04 15:41:26 +0700

    Added a comment on class TutorialModClient.java

--- src/main/java/net/botred/tutorialmod/TutorialModClient.java
+++ src/main/java/net/botred/tutorialmod/TutorialModClient.java
@@ -1,7 +1,7 @@
 package net.botred.tutorialmod;
 
 import net.fabricmc.api.ClientModInitializer;
-
+// idk what im doing but im adding a comment i think
 public class TutorialModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
